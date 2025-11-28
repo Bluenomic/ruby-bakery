286637eecaf161ec1447b0c41c53a69144cbdbf6
Imam Dzulvan M <11231031@student.itk.ac.id>
2025-11-28 17:24:54 +0800

    Config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ruby-bakery/', 
+  base: 'ruby-bakery', 
   plugins: [
     vue(),
     vueDevTools(),
